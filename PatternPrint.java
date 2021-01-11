8f6528f338df08e008ef1e92e7138d1973a99345
Mathanjeevika <Mathanjeevika@users.noreply.github.com>
2021-01-11 11:30:23 +0530

    Update PatternPrint.java

--- PatternPrint.java
+++ PatternPrint.java
@@ -29,11 +29,11 @@ public class PatternPrint {
 		for(int i = value;i>0;i--) {
 			//to print the values other that 
 			for(int j = value;j>i;j--)
-				System.out.print(j);
+				System.out.print(j+" ");
 			
 			//to print the row value which is same
 			for(int j = i;j>0;j--)
-				System.out.print(i);
+				System.out.print(i+" ");
 			
 			System.out.println();
 		}
